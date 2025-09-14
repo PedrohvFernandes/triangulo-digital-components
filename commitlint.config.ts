f79fda956f917ee855090373d22de867dcbf3c9e
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2025-09-14 12:11:29 -0300

    chore: preparar aplicação para publicar

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,1 +1,12 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 100],
+    'scope-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore'],
+    ],
+  },
+}
