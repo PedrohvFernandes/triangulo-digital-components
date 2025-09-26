bf865b16d6b6e74f59bab068f2fd7f2007838189
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2025-09-26 14:09:43 -0300

    refactor: otimizar lib p/ usar apenas ESM e importar só o necessário em cada uso

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 // Arquivo gerado automaticamente
 
+// Exporta todos os componentes (incluindo provider dentro de triangulo-digital)
 export * from './components'
+
+// Exporta hooks
 export * from './hooks'
+
+// Exporta libs
 export * from './lib'
